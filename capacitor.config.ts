c06fc9e03af1814ac80e1bc81b365d90b6ca3c4a
langaran-y <youseflangaran65@gmail.com>
2024-10-29 13:55:24 +0330

    add android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ir.promal.app',
+  appName: 'promal',
+  webDir: 'out'
+};
+
+export default config;
